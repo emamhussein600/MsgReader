a1e0eeae29f3e1ad656129860a54a398fe5548f6
emamhussein600 <emamhussein600@gmail.com>
2025-08-14 11:25:35 +0300

    add new method

--- src/main/java/ManagedBean/MsgReaderBean.java
+++ src/main/java/ManagedBean/MsgReaderBean.java
@@ -41,7 +41,7 @@ public class MsgReaderBean implements Serializable {
 
     private List<EmailData> list;
 
-    public void msgReadFrom() {
+    public EmailData msgReadFrom() {
         if (uploadedFile != null) {
             try {
                 byte[] fileBytes = uploadedFile.getContent(); // get the file content
@@ -67,7 +67,7 @@ public class MsgReaderBean implements Serializable {
                                 try {
                                     String filePath = desktopPath + fa.getLongFilename();
                                     Files.write(Paths.get(filePath), fa.getData());
-                                    System.out.println("Saved attachment: " + filePath);
+                                    //System.out.println("Saved attachment: " + filePath);
                                     return new AttachmentData(fa.getLongFilename(), new ByteArrayInputStream(fa.getData()));
                                 } catch (IOException e) {
                                     e.printStackTrace();
@@ -79,52 +79,51 @@ public class MsgReaderBean implements Serializable {
                         }).collect(Collectors.toList());
                 emailData.setAttachments(attachmentList);
 
-                // Print attachments
-                if (emailData.getAttachments() != null) {
-                    System.out.println("Attachments:");
-                    for (AttachmentData attData : emailData.getAttachments()) {
-                        System.out.println("Filename: " + attData.getFileName());
+//                // Print attachments
+//                if (emailData.getAttachments() != null) {
+//                    System.out.println("Attachments:");
+//                    for (AttachmentData attData : emailData.getAttachments()) {
+//                        System.out.println("Filename: " + attData.getFileName());
+//
+//                        if (attData.getFileContent() != null) {
+//                            try {
+//                                // Read content from stream (Java 8 compatible)
+//                                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+//                                byte[] data = new byte[1024];
+//                                int nRead;
+//                                while ((nRead = attData.getFileContent().read(data, 0, data.length)) != -1) {
+//                                    buffer.write(data, 0, nRead);
+//                                }
+//                                buffer.flush();
+//                                byte[] contentBytes = buffer.toByteArray();
+//                                System.out.println("Content length: " + contentBytes.length + " bytes");
+//
+//                                // Optional: recreate InputStream if you want to reuse it
+//                                attData.setFileContent(new ByteArrayInputStream(contentBytes));
+//
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+//                        } else {
+//                            System.out.println("No content available.");
+//                        }
+//                    }
+//                }
 
-                        if (attData.getFileContent() != null) {
-                            try {
-                                // Read content from stream (Java 8 compatible)
-                                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                                byte[] data = new byte[1024];
-                                int nRead;
-                                while ((nRead = attData.getFileContent().read(data, 0, data.length)) != -1) {
-                                    buffer.write(data, 0, nRead);
-                                }
-                                buffer.flush();
-                                byte[] contentBytes = buffer.toByteArray();
-                                System.out.println("Content length: " + contentBytes.length + " bytes");
-
-                                // Optional: recreate InputStream if you want to reuse it
-                                attData.setFileContent(new ByteArrayInputStream(contentBytes));
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        } else {
-                            System.out.println("No content available.");
-                        }
-                    }
-                }
-
-                // Optional: print email info in console
-                System.out.println("Subject: " + emailData.getSubject());
-                System.out.println("Body: " + emailData.getBody());
-                System.out.println("From email: " + emailData.getSender());
-                if (emailData.getToList() != null) {
-                    System.out.println("To:");
-                    emailData.getToList().forEach(t -> System.out.println(" - " + t));
-                }
-                if (emailData.getCcList() != null) {
-                    System.out.println("Cc:");
-                    emailData.getCcList().forEach(c -> System.out.println(" - " + c));
-                }
+//                // Optional: print email info in console
+//                System.out.println("Subject: " + emailData.getSubject());
+//                System.out.println("Body: " + emailData.getBody());
+//                System.out.println("From email: " + emailData.getSender());
+//                if (emailData.getToList() != null) {
+//                    System.out.println("To:");
+//                    emailData.getToList().forEach(t -> System.out.println(" - " + t));
+//                }
+//                if (emailData.getCcList() != null) {
+//                    System.out.println("Cc:");
+//                    emailData.getCcList().forEach(c -> System.out.println(" - " + c));
+//                }
 
                 System.out.println("MSG file processed successfully!");
-                System.out.println("The Object is : " + emailData.toString());
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -132,6 +131,7 @@ public class MsgReaderBean implements Serializable {
         } else {
             System.out.println("No file uploaded!");
         }
+        return emailData;
     }
 
     // Helper Method To Extract CC And To List Emails
@@ -168,7 +168,7 @@ public class MsgReaderBean implements Serializable {
     }
 
     public void print() {
-        System.out.println("The Object is : " + emailData.toString());
+        System.out.println("The object is From Print method : "+msgReadFrom());
     }
 
     public void setUploadedFile(UploadedFile uploadedFile) {
